459b034a9411ce82301795b4a3bf43f99f9ea6c1
SpiSpa <133459316+SpiSpa@users.noreply.github.com>
2025-01-09 14:48:07 -0600

    added functionality

--- game.js
+++ game.js
@@ -1,12 +1,31 @@
 var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var userClickedPattern = [];
+level = 0;
 
 function nextSequence(){
 
+    userClickedPattern = []; //resets the user input to an empty array.
     var randomNumber = Math.floor(Math.random() * 4);
-    console.log("Curent random number: " + randomNumber);
     var randomChosenColor = buttonColors[randomNumber];
-    return randomChosenColor;
+    gamePattern.push(randomChosenColor);
+    makeSound(randomChosenColor);
+    buttonFlash(randomChosenColor);
+    console.log("current random color: " + randomChosenColor);
+    console.log("random color array: " + gamePattern);
+
+    // ******************this part's not working ********************
+    //**********instead of only playing the most recent color, I want it to play all of them.
+
+   /* for (let i = 0; i < gamePattern.length; i++){
+        console.log("iteration " + i);
+        buttonFlash(gamePattern[i]);
+        setTimeout(function(){
+            buttonFlash(gamePattern[i+1])
+        }, 500);
+        
+    }*/
+
 }
 
 // play the sound corresponding with each button color
@@ -14,25 +33,21 @@ function makeSound(randomChosenColor){
     switch (randomChosenColor){
         case "red":
             var redSound = new Audio("sounds/red.mp3");
-            console.log("red sound played");
             redSound.play();
             break;
 
         case "blue":
             var blueSound = new Audio("sounds/blue.mp3");
-            console.log("blue sound played");
             blueSound.play();
             break;
 
         case "green":
             var greenSound = new Audio("sounds/green.mp3");
-            console.log("green sound played");
             greenSound.play();
             break;
 
         case "yellow":
             var yellowSound = new Audio("sounds/yellow.mp3");
-            console.log("yellow sound played");
             yellowSound.play();
             break;
 
@@ -40,48 +55,51 @@ function makeSound(randomChosenColor){
     }
 }
 
-function letsStart(){
-
+function buttonFlash(color){
    
-    randomChosenColor = nextSequence();
-    console.log("chosen color: " + randomChosenColor);
-
     //dim out chosen button
-    $("#" + randomChosenColor).addClass("pressed");
+    $("#" + color).addClass("pressed");
 
     // make the corresponding sound
-    makeSound(randomChosenColor);
+    makeSound(color);
 
     // undim the chosen button to make it look like it flashed.
     setTimeout(function(){
-        $("#" + randomChosenColor).removeClass("pressed")
-        }, 500);
+        $("#" + color).removeClass("pressed")
+        }, 200);
 }
 
+function checkCorrect(gamePattern, userClickedPattern){
+    gameCheck = gamePattern.join();  //enhance this part by finding an incorrect match 
+    userCheck = userClickedPattern.join();   // as soon as the user makes a mistake.
+    
+    if (gameCheck == userCheck) {
+        console.log("computer and user match");
+    }
+    else {
+        console.log("computer and user DO NOT match");
+        $("h1").text("GAME OVER");
+        $("h1").css("color", "red");
+    }
+}
+
+
 $(".btn").click(function() {
-    $("h1").css("color", "green");
-    letsStart();
-});
-/*
-for (i = 0; i < 5; i++){
+    var userChosenColor = this.id
+    makeSound(userChosenColor);
+    buttonFlash(userChosenColor);
+    
+    userClickedPattern.push(userChosenColor);
 
-    console.log("test " + i + ":");
-    randomChosenColor = nextSequence();
-    console.log(randomChosenColor);
+    if (userClickedPattern.length == gamePattern.length) {
+        checkCorrect(gamePattern, userClickedPattern);
+    }
 
-    gamePattern.push(randomChosenColor);
-    console.log(gamePattern);
+    });
 
-    $("#" + randomChosenColor).addClass("pressed");
-
-    setTimeout(function(){
-        $("#" + randomChosenColor).removeClass("pressed")
-    }, 500);
-
-
-
-   // for (i = 0; i < length.gamePattern; i++){
-    //    $("#" + gamePattern[i]).addClass(".pressed")
-   // } 
-    //
-}*/
+$(document).keydown(function(){
+    console.log("key pressed")
+    nextSequence();
+    $("h1").text("level " + (level + 1));
+    level++;
+    });
